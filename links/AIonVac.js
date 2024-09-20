c388f7c28460a60081fe9eb0b08d18964b247b9f
soulymoon <47885549+soulymoon@users.noreply.github.com>
2024-09-20 10:55:57 +0200

    Add files via upload
    ai on vacation added

---
+++ links/AIonVac.js
@@ -0,0 +1,96 @@
+var nImgNum_pg_7 = 0;
+      wp_imgArray_pg_7 = new Array();
+	  wp_imgArray_pg_7[nImgNum_pg_7++] = new wp_galleryimage("wpimages/AI_1.png", 739, 493, "wpimages/AI_1.png", "");
+	  wp_imgArray_pg_7[nImgNum_pg_7++] = new wp_galleryimage("wpimages/AI_2-1.png", 739, 493, "wpimages/AI_2-1.png", "");
+	  wp_imgArray_pg_7[nImgNum_pg_7++] = new wp_galleryimage("wpimages/AI_2.png", 739, 493, "wpimages/AI_2.png", "");
+	  wp_imgArray_pg_7[nImgNum_pg_7++] = new wp_galleryimage("wpimages/AI_3.png", 739, 493, "wpimages/AI_3.png", "");
+	  wp_imgArray_pg_7[nImgNum_pg_7++] = new wp_galleryimage("wpimages/AI_4.png", 739, 493, "wpimages/AI_4.png", "");
+	  wp_imgArray_pg_7[nImgNum_pg_7++] = new wp_galleryimage("wpimages/AI_5.png", 739, 493, "wpimages/AI_5.png", "");
+	  wp_imgArray_pg_7[nImgNum_pg_7++] = new wp_galleryimage("wpimages/AI_6.png", 739, 493, "wpimages/AI_6.png", "");
+	  wp_imgArray_pg_7[nImgNum_pg_7++] = new wp_galleryimage("wpimages/AI_7.png", 739, 493, "wpimages/AI_7.png", "");
+	  wp_imgArray_pg_7[nImgNum_pg_7++] = new wp_galleryimage("wpimages/AI_8.png", 739, 493, "wpimages/AI_8.png", "");
+	  wp_imgArray_pg_7[nImgNum_pg_7++] = new wp_galleryimage("wpimages/AI_9.jpg", 739, 493, "wpimages/AI_9.jpg", "");
+	  wp_imgArray_pg_7[nImgNum_pg_7++] = new wp_galleryimage("wpimages/AI_10.jpg", 739, 493, "wpimages/AI_10.jpg", "");
+      $(document).ready(function() {
+      
+$('#wp_gallery_pg_7').wpgallery({
+		imageArray: wp_imgArray_pg_7,
+		nTotalImages: nImgNum_pg_7,
+		nGalleryLeft: 24,
+		nGalleryTop: 217,
+		nGalleryWidth: 1920,
+		nGalleryHeight: 1040,
+		nImageDivTop: 0,
+		nImageDivWidth: 700,
+		nImageDivHeight: 600,
+		nControlBarStyle: 0,
+		nControlBarExternalTop: 10,
+		bNavBarOnTop: true,
+		bShowNavBar: false,
+		nNavBarAlignment: 0,
+		strNavBarColour: 'none',
+		nNavBarOpacity: 1.0,
+		nNavBarIconWidth: 24,
+		nNavBarIconHeight: 24,
+		bShowCaption: false,
+		bCaptionCount: true,
+		strCaptionColour: '#ffffff',
+		nCaptionOpacity: 0.6,
+		strCaptionTextColour: '#000000',
+		nCaptionFontSize: 12,
+		strCaptionFontType: 'Courier New,Arial,_sans',
+		strCaptionAlign: 'center',
+		strCaptionFontWeight: 'normal',
+		bShowThumbnails: false,
+		nThumbStyle: 1,
+		nThumbPosition: 0,
+		nThumbLeft: 30,
+		nThumbTop: 483,
+		nThumbOpacity: 0.5,
+		nTotalThumbs: 5,
+		nThumbSize: 40,
+		nThumbSpacing: 10,
+		bThumbBorder: true,
+		strThumbBorderColour: '#000000',
+		strThumbBorderHoverColour: '#ffffff',
+		strThumbBorderActiveColour: '#ffffff',
+		bShowThumbnailArrows: true,
+		nThumbButtonSize: 24,
+		nThumbButtonIndent: 50,
+		nColBorderWidth: 2,
+		nTransitionStyle: 1,
+		nStaticTime: 2500,
+		nTransitTime: 1200,
+		bAutoplay: true,
+		loadingButtonSize: 38,
+		bPageCentred: false,
+		nPageWidth: 1098,
+		nZIndex: 100,
+		loadingButtonSrc: 'wpimages/wpgallery_loading_1.gif',
+		blankSrc: 'wpscripts/blank.gif',
+		rewindButtonSrc: 'wpimages/wpgallery_rewind_0.png',
+		prevButtonSrc: 'wpimages/wpgallery_previous_0.png',
+		playButtonSrc: 'wpimages/wpgallery_play_0.png',
+		pauseButtonSrc: 'wpimages/wpgallery_pause_0.png',
+		nextButtonSrc: 'wpimages/wpgallery_next_0.png',
+		forwardButtonSrc: 'wpimages/wpgallery_forward_0.png',
+		thumbRewindButtonSrc: 'wpimages/wpgallery_rewind_0.png',
+		thumbForwardButtonSrc: 'wpimages/wpgallery_forward_0.png',
+		rewindoverButtonSrc: 'wpimages/wpgallery_rewind_over_0.png',
+		prevoverButtonSrc: 'wpimages/wpgallery_previous_over_0.png',
+		playoverButtonSrc: 'wpimages/wpgallery_play_over_0.png',
+		pauseoverButtonSrc: 'wpimages/wpgallery_pause_over_0.png',
+		nextoverButtonSrc: 'wpimages/wpgallery_next_over_0.png',
+		forwardoverButtonSrc: 'wpimages/wpgallery_forward_over_0.png',
+		thumboverRewindButtonSrc: 'wpimages/wpgallery_rewind_over_0.png',
+		thumboverForwardButtonSrc: 'wpimages/wpgallery_forward_over_0.png',
+		strRewindToolTip: 'Reverse',
+		strPreviousToolTip: 'Previous',
+		strPlayToolTip: 'Play',
+		strPauseToolTip: 'Pause',
+		strNextToolTip: 'Next',
+		strForwardToolTip: 'Forward',
+		strThumbRewindToolTip: 'Reverse',
+		strThumbForwardToolTip: 'Forward'
+      });
+      });
